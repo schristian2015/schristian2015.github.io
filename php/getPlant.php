f58cc036ece1ed7066f07f3fe23cd8dda64b44de
schristian2015 <schristian2015@fau.edu>
2019-04-18 09:11:30 -0400

    v1 update test

--- php/getPlant.php
+++ php/getPlant.php
@@ -19,6 +19,7 @@ if ($result->num_rows > 0) {
 	
     // output data of each row
     while($row = $result->fetch_assoc()) {
+        ob_end_clean();
         echo(json_encode($row));
     }
 } else {
